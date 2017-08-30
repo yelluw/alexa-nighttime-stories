711e506d0160b4bb5b15149a0148083aabdbec3e
Yelluw <pr@yelluw.com>
2017-08-30 01:18:56 -0400

    v1.0.0 completed

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # alexa-nighttime-stories
 Alexa skill that reads nighttime stories. 
+
+
+
+
+#### Dev portal settings
+
+Requires `ngrok`
+
+- Make sure the HTTPS radio button is selected for the "Endpoint" field.
+
+- Enter the HTTPS endpoint from ngrok into the textfield.
+
+- Don't bother with "Account Linking".
+
+
+
+It's important to choose the second radio button with the label because that's what ngrok uses.
+
+**My development endpoint is a subdomain of a domain that has a wildcard certificate from a certificate authority.**
+
+
+#### Tools
+
+Test with online emulator: https://echosim.io/
--- main.py
+++ main.py
@@ -1,19 +1,40 @@
-"""
-Hello world in alexa. 
-"""
-
-
-
+import logging
+import json
 from flask import Flask, render_template
-from flask_ask import Ask, statement
+from flask_ask import Ask, statement, question, session
+
 
 app = Flask(__name__)
-ask = Ask(app, '/')
+ask = Ask(app, "/")
 
-@ask.intent('HelloIntent')
-def hello(firstname):
-    text = render_template('hello', firstname=firstname)
-    return statement(text).simple_card('Hello', text)
+logging.getLogger("flask_ask").setLevel(logging.DEBUG)
+
+
+@ask.launch
+def welcome():
+    """
+    Message that runs on app launch
+    """
+    return question(render_template('welcome'))
+
+
+@ask.intent("ReadStoryIntent")
+def read_story():
+    """
+    Returns the day's story.
+    """
+    story = None
+
+    with open("story.json", "r") as f:
+        story = json.loads(f.read())
+        print(story)
+
+    if not story:
+        return statement('story_error')
+
+    story = render_template('story', title=story.get('title'), body=story.get('body'))
+    return statement(story)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
